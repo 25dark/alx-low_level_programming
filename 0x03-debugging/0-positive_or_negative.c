8c71010d5c0d339ae64894374d752b8f08e140a1
Henry Danso <henrydanso6@gmail.com>
2022-09-16 04:06:58 +0000

    Update 0-positive_or_negative.c

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -3,29 +3,29 @@
 #include <time.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: 'the program's description'
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int n;
+	int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					if (n > 0)
-							{
-										printf("%d is positive\n", n);
-											}
-						if (n == 0)
-								{
-											printf("%d is zero\n", n);
-												}
-							if (n < 0)
-									{
-												printf("%d is negative\n", n);
-													}
-								return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
