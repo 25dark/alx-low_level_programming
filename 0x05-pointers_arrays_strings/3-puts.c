57486b784b07094fb4a6adbd728debc4de9f0f54
Henry Danso <henrydanso6@gmail.com>
2022-09-19 23:52:47 +0000

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 #include "2-strlen.c"
 /**
- *  * _puts - puts a string
- *   *
- *    * @str: string to print
- *     */
+ * _puts - puts a string
+ *
+ * @str: string to print
+ */
 void _puts(char *str)
 {
-		int i;
+	int i;
 
-			for (i = 0; *(str + i) != '\0'; i++)
-					{
-								_putchar(*(str + i));
-									}
-				_putchar('\n');
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
 }
