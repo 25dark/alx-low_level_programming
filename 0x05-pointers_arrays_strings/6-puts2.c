0e6e86d16db4eecc638f74600b33eb98f4dc28a7
Henry Danso <henrydanso6@gmail.com>
2022-09-20 02:59:42 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 #include "2-strlen.c"
 /**
- *  * puts2 - puts string twice
- *   *
- *    * @str: string to put twice
- *     */
+ * puts2 - puts string twice
+ * @str: string to put twice
+ */
 void puts2(char *str)
 {
-		int i;
+	int i;
 
-			for (i = 0; i < _strlen(str); i++)
-					{
-								if (i % 2 == 0)
-											{
-															_putchar(str[i]);
-																	}
-									}
-				_putchar('\n');
+	for (i = 0; i < _strlen(str); i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
