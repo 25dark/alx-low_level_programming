0a7e32ad01d6383813dba1239b9d9c01a15cb97d
Henry Danso <henrydanso6@gmail.com>
2022-09-20 00:01:50 +0000

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 #include "2-strlen.c"
 /**
- *  * print_rev - reverse a string
- *   *
- *    * @s: string to print
- *     */
+ * print_rev - reverse a string
+ *
+ * @s: string to print
+ */
 void print_rev(char *s)
 {
-		int i;
+	int i;
 
-			for (i = _strlen(s) - 1; i >= 0; i--)
-					{
-								_putchar(*(s + i));
-									}
-				_putchar('\n');
+	for (i = _strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
 }
